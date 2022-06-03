73dfcb4e56ba461e3acd9abb84298e15aca0b199
DENIS BLINOV <blinovda1@mail.ru>
2022-06-03 14:50:58 +0300

    add task 1

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,55 +1,18 @@
-﻿void Print(int[,] arr)
-{
-    int row_size = arr.GetLength(0);
-    int column_size = arr.GetLength(1);
+﻿// Пользователь вводит с клавиатуры M чисел.
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
 
-    for (int i = 0; i < row_size; i++)
+int CountNum()
+{
+    int count = 0;
+    string word;
+
+    while (true)
     {
-        for (int j = 0; j < column_size; j++)
-        {
-            Console.Write($" {arr[i, j]} ");
-        } 
-        Console.WriteLine();       
+        Console.Write("Output any number: ");
+        word = Console.ReadLine();
+
+        if (word == "") return count;
+        else if (int.Parse(word) > 0) count += 1;
     }
-    Console.WriteLine();
 }
-
-int[,] MassNums(int row, int column, int from, int to)
-{
-    int[,] arr = new int[row, column];
-
-    for (int i = 0; i < row; i++)
-    {   
-        for (int j = 0; j < column; j++)
-        {
-            arr[i, j] = new Random().Next(from, to);
-        }        
-    }
-    return arr;
-}
-
-int[,] CopyMass(int[,] arr)
-{
-    int row_size = arr.GetLength(0);
-    int column_size = arr.GetLength(1);
-    int[,] new_arr = new int[row_size, column_size];
-    
-    for (int i = 0; i < row_size; i++)
-    {
-        for (int j = 0; j < column_size; j++)
-        {
-            new_arr[i, j] = arr[i, j];
-        }        
-    }
-    return new_arr;
-}
-
-int[,] arr_1 = MassNums(2, 5, 1, 11);
-Print(arr_1);
-int[,] arr_1_new = CopyMass(arr_1);
-Print(arr_1_new);
-
-int[,] arr_2 = MassNums(3, 6, 10, 21);
-Print(arr_2);
-int[,] arr_2_new = CopyMass(arr_2);
-Print(arr_2_new);
+Console.WriteLine($"You entered {CountNum()} numbers greater than 0");
